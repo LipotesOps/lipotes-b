462dcac3b78e7432365a8b324e640a034de08c6f
Corwin Zhang <yanbing2020@gmail.com>
2020-07-09 23:46:55 +0800

    add simple jwt

--- config/settings.py
+++ config/settings.py
@@ -52,17 +52,24 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.TokenAuthentication',
     ],
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema", # 新添加
     'PAGE_SIZE': 10
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=7),
 }
 
 # jwt载荷中的有效期设置
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-# jwt内部实现的登陆视图
-from rest_framework_jwt.views import obtain_jwt_token
+
+from rest_framework_simplejwt import views as jwt_views
 from . import views
 
 urlpatterns=[
-    path(r"login", views.UserLoginAPIView.as_view()),
-    path(r"logout", views.UserLogoutAPIView.as_view()),
-    path(r'register', views.registration, name='register')
+    path(r"login/", views.UserLoginAPIView.as_view()),
+    path(r"logout/", views.UserLogoutAPIView.as_view()),
+    path(r'register/', views.registration, name='register'),
+    path(r'token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(r'token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
 ]
